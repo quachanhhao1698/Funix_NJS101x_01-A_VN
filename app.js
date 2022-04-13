292c843c116c66a7cc329c11e80015d8e1a48d61
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-13 14:52:03 +0700

    Routing Request 1

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
       console.log(req.url, req.method, req.headers);
+      const url = req.url;
+      if(url === '/') {
+            res.setHeader('Content-Type', 'text/html');
+            res.write(
+                  `<html>
+                        <head>
+                              <title>Enter Message</title>
+                        </head>
+                        <body>
+                              <form action="/message" method="POST">
+                              <input type="text" name="message">
+                              <button type="submit">Send</button>
+                              </form>
+                        </body>
+                  </html>
+                        `);
+            
+            return res.end(); 
+      }
       res.setHeader('Content-Type', 'text/html');
       res.write(
             `<html>
@@ -14,7 +33,7 @@ const server = http.createServer((req, res) => {
             </html>
                   `);
       
-      res.end();
+      res.end(); 
 
 });
 
