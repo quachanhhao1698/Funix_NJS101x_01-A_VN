6efc44df40ea9dab94fafd8a3720eda4ac540cdc
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-13 13:41:13 +0700

    Send Response

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-      console.log(req);
+      console.log(req.url, req.method, req.headers);
+      res.setHeader('Content-Type', 'text/html');
+      res.write(
+            `<html>
+                  <head>
+                        <title>App with NodeJS</title>
+                  </head>
+                  <body>
+                        <h1>Hello from my Node.js server !</h1>
+                  </body>
+            </html>
+                  `);
+      
+      res.end();
+
 });
 
 server.listen(3000)
