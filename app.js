62c4c7c11ae995b5d829a4cef743a7284aca90ba
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-15 01:07:18 +0700

    How Middleware Works

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express();
 
 app.use((req, res, next) => {
       console.log('In the middleware!');
-      next();
+      next(); //cho phép request tiếp tục đến dòng middleware tiếp theo
 });
 
 app.use((req, res, next) => {
       console.log('In another middleware!');
+      res.send('<h1>Hello from Express !</h1>');
 });
 
 const server = http.createServer(app);
