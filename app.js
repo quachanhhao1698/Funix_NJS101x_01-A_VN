3886ac844cc0343ea96f35acaa289f2b494fda09
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-15 09:44:32 +0700

    Using Express Router

--- app.js
+++ app.js
@@ -3,28 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) => {
-      console.log('In another middleware!');
-      res.send(`
-            <form action="/product" method="POST">
-                  <input type="text" name="title">
-                  <button type="submit">Add Product</button>
-            </form>
-      `);
-});
-
-app.post('/product', (req, res, next) => {
-      console.log(req.body);
-      res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-      console.log('In another middleware!');
-      res.send('<h1>Hello from Express !</h1>');
-});
 
 const server = http.createServer(app);
 
