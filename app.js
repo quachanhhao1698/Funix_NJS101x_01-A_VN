ce6d2a2679338ca3db94857ca4af2e7c5874cc2b
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-15 09:53:38 +0700

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next)=>{
+      res.status(404).send("<h1>Page not found 404</h1>");
+});
+
 
 const server = http.createServer(app);
 
