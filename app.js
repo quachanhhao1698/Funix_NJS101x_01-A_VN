8543e6c8e0b9f923c26c150b62aec56b2be52e93
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-16 20:26:05 +0700

    Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const rootdir = require('./util/path')
 
 
 const app = express();
@@ -12,7 +13,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-      res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+      res.status(404).sendFile(path.join(rootdir, 'views', '404.html'));
 });
 
 app.listen(3000);
