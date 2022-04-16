d58985c6e6db07976328a77c4169eb499d13623d
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-16 16:13:12 +0700

    Returning a 404 Page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 const adminRoutes = require('./routes/admin');
@@ -11,10 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
-      res.status(404).send("<h1>Page not found 404</h1>");
+      res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
