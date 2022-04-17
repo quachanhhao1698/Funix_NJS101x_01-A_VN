d59ca874805f097c3a306a3dcf49616f280551c2
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-18 01:54:56 +0700

    Adding the Layout to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,14 @@ const adminData = require('./admin');
 
 router.get('/',(req, res, next) => {
       const product = adminData.products;
-     res.render('shop', {prods: product,pageTitle: 'Shop',path: '/', hasProducts: product.length > 0});
+     res.render('shop',
+     {prods: product,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: product.length > 0,
+      activeShop: true,
+      formCSS: true,
+      productCSS: true});
 });
 
 module.exports = router
