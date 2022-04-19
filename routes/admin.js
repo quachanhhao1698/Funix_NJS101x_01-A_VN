7297fe4b0ab05c6279065f7c42299c78021dec55
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-20 03:41:48 +0700

    Adding a Product Model

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,15 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
 const router = express.Router();
-const rootdir = require('../util/path');
 
-const products = [];
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product',(req, res, next) => {
-      // console.log('In another middleware!');
-      res.render('add-product', {
-            pageTitle : 'Add Product',
-            path: '/admin/add-product',
-            activeProduct: true,
-            formCSS: true,
-            productCSS: true})
-});
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-      console.log(req.body);
-      products.push({title: req.body.title})
-      console.log('Arr Products',products);
-      res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
