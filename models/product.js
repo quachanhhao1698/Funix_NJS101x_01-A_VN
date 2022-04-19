3d6df5b4323a864204ace10d02f43e5f86c50d2e
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-20 04:09:42 +0700

    Adding a Product Model

--- models/product.js
+++ models/product.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const products = [];
 
 module.exports = class Product {
     constructor(t) {
@@ -7,26 +6,10 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent)=> {
-            let products =[];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=> {
-                console.log(err);
-            });
-        });
+        products.push(this);
     }
 
     static fetchAll() {
-        const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent)=> {
-            if(err){
-                return [];
-            }
-            return JSON.parse(fileContent);
-        })
+        return products;
     }
 }
