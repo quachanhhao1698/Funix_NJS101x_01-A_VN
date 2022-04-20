5e486a743e22c1822fef6ac405d6a7a6199bce34
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-20 14:57:03 +0700

    Rendering the Product Detail View

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,8 +14,8 @@ exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId,product => {
     console.log('Product info:',product);
+    res.render('shop/product-detail',{product: product,pageTitle: product.title, path: '/products'});
   });
-  res.redirect('/');
 }
 
 exports.getIndex = (req, res, next) => {
