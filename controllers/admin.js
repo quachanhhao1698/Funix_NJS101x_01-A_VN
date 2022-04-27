07b7bd8bd8b1a6f28fbf12d0a462ff21a581c1ef
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-04-27 16:47:59 +0700

    Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -86,6 +86,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect("/admin/products");
+    Product.findByPk(prodId)
+        .then(product => {
+            return product.destroy();
+        })
+        .then(result => {
+            console.log('Mess_postDeleteProduct: DELETED');
+            res.redirect("/admin/products");
+        })
+        .catch(err => console.log('ERROR_postDeleteProduct: ', err))
 };
