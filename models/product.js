83d8c7b67a281a84c1256fef12c29e678ef7bbb2
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-05-02 22:33:16 +0700

    Creating the Database Connection

--- models/product.js
+++ models/product.js
@@ -1,30 +1,39 @@
-const Sequelize = require("sequelize");
+const mongoConnect = require('../util/database')
 
-const sequelize = require("../util/database");
-
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false
-  },
-  price: {
-    type: Sequelize.DataTypes.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.DataTypes.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+
+  save() {}
+}
+
+// const Product = sequelize.define("product", {
+//   id: {
+//     type: Sequelize.DataTypes.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: {
+//     type: Sequelize.DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   price: {
+//     type: Sequelize.DataTypes.DOUBLE,
+//     allowNull: false,
+//   },
+//   imageUrl: {
+//     type: Sequelize.DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.DataTypes.STRING,
+//     allowNull: false,
+//   },
+// });
 
 module.exports = Product;
