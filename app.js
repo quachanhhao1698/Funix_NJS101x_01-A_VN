d9cfd64563aad9b410ea190af129af6418a0a6e6
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-05-02 22:18:08 +0700

    Installing the MongoDB Driver

--- app.js
+++ app.js
@@ -4,67 +4,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
+const mongoConnect = require('./util/database');
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Tạo liên kết giữa các bảng
-Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" }); //Product thuộc về User
-User.hasMany(Product); // 1 User có nhiều Product
-User.hasOne(Cart); // 1 User có 1 Cart
-Cart.belongsTo(User); // Cart thuộc về User
-Cart.belongsToMany(Product, { through: CartItem }); // Cart thuộc về nhiều Product -> 1 SP có thể nằm trong nhiều giỏ hàng
-Product.belongsToMany(Cart, { through: CartItem }); // Product thuộc về nhiều Cart -> 1 giỏ hàng có thể chứa nhiều SP
-Order.belongsTo(User)
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+mongoConnect( client => {
+  console.log(client);
+  app.listen(3000);
+});
 
-sequelize
-//   .sync({force: true})
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Max", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => console.log(err));
+
--- util/database.js
+++ util/database.js
@@ -1,5 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '01245312', {dialect: 'mysql', host: 'localhost'} );
+const mongoConnect = callback => {
 
-module.exports = sequelize;
+  MongoClient.connect(
+    "mongodb+srv://quachanhhao:01245312z@cluster0.zs7t6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
