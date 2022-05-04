61c050b102adf9107df2bd4df386aef9f22cadfb
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-05-04 19:25:35 +0700

    Storing Multiple Products in the Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -80,6 +80,7 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
+      res.redirect('/cart')
     })
     .catch((err) => {
       console.log(err);
