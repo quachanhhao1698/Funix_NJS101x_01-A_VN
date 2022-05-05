cf4917bbcc1e095328fdbcee8ad912a831f94eb4
Quach Anh Hao <quachanhhao1698@gmail.com>
2022-05-05 20:36:12 +0700

    Fetching All Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({})
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
@@ -39,7 +39,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({})
     .then((products) => {
       // console.log("index_product: ", products);
       res.render("shop/index", {
